22848b9e2ccf5ddca57a1d94ea90e57f8ff58234
unknown <ritapaska@gmail.com>
2023-04-20 16:12:19 +0200

    hiba javitas

--- Jatek/server/db.php
+++ Jatek/server/db.php
@@ -6,6 +6,7 @@ $conn;
 try {
   $conn = new PDO("mysql:host=$servername;dbname=jatekok", $username, $password);
   // set the PDO error mode to exception
+  $conn-> exec("set names utf8");
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
